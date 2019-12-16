bacf51fb17baae3a2eb951e70a513b8eabc8b3ab
Marcelo Campos Silva <Marcelo.Campos@ntconsult.net>
2019-12-16 14:39:18 -0200

    Paradigma funcional

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -12,7 +12,17 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.split('-')); // ou new Date(this._inputData.value.replace(/-/g, ',')); trocar o '-' por virgúla. o g é globlal todas as ocorencias daquele caractere
+        //let data = new Date(this._inputData.value.split('-')); ou new Date(this._inputData.value.replace(/-/g, ',')); trocar o '-' por virgúla. o g é globlal todas as ocorencias daquele caractere
+        let data = new Date(
+            ...this._inputData.value.split('-').map(function (item, indice) {
+                return item - indice % 2; // Modulo = % .Exemplo: 0 % 2 = 0; 1 % 2 = 1; 2 % 2 = 0;
+                //Ou 
+                // if(indice == 1){
+                //     return item - 1;
+                // }
+                //return item;
+            })
+        ); // Os "..." serem para referenciar as posições do array que o split gera então . = 1, .. = 2 e ... = 3
         console.log(data);
         
         // let negociacao = new Negociacao(
