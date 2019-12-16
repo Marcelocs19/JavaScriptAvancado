7b60de1801294f16d87cd103da270d92ffb60bc9
Marcelo Campos Silva <Marcelo.Campos@ntconsult.net>
2019-12-16 14:48:38 -0200

    Arrow Functions

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -14,22 +14,21 @@ class NegociacaoController {
 
         //let data = new Date(this._inputData.value.split('-')); ou new Date(this._inputData.value.replace(/-/g, ',')); trocar o '-' por virgúla. o g é globlal todas as ocorencias daquele caractere
         let data = new Date(
-            ...this._inputData.value.split('-').map(function (item, indice) {
-                return item - indice % 2; // Modulo = % .Exemplo: 0 % 2 = 0; 1 % 2 = 1; 2 % 2 = 0;
+            ...this._inputData.value.split('-').map((item, indice) => item - indice % 2)//{ trocado o function por => chamado de Arrow Functions e não precisa usar o return
+                //return item - indice % 2; // Modulo = % .Exemplo: 0 % 2 = 0; 1 % 2 = 1; 2 % 2 = 0;
                 //Ou 
                 // if(indice == 1){
                 //     return item - 1;
                 // }
                 //return item;
-            })
+            //})
         ); // Os "..." serem para referenciar as posições do array que o split gera então . = 1, .. = 2 e ... = 3
-        console.log(data);
-        
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     this._inputQuantidade.value,
-        //     this._inputValor.value
-        // );
-        // console.log(negociacao);        
+                
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+        console.log(negociacao);        
     }
 }
